33e2709909d41d394fd52eeb840d28e10655b28c
buse.odaci <odacbuse@gmail.com>
2020-05-20 23:43:06 +0300

    introduction,

---
+++ Python/Introduction/Arithmetic Operators/Solution.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 20 23:29:33 2020
+
+@author: buse
+"""
+
+
+number1 = int(input())
+number2 = int(input())
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
---
+++ Python/Introduction/Division/Solution.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 20 23:39:23 2020
+
+@author: buse
+"""
+
+
+a = int(input())
+b = int(input())
+print(a // b)
+print(a / b)
